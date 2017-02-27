8acbfa18629dc64c608d063ad3f9e2842e2cbcc2
John Tran <jptran318@gmail.com>
2017-02-26 16:25:54 -0800

    App fully functional

--- readme.md
+++ readme.md
@@ -4,5 +4,9 @@ Project to self-study React/Redux.
 Based on user input, checks the weather forecast of an area for the
 next 5 days.
 
+## TODO
+* Add option to change temperature units
+* Ability to remove entries
+* Save entries upon refresh
 
 Course content can be found [here](https://www.udemy.com/react-redux/)
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,11 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) { // initial state is empty array
   // redux-promise receives AJAX request, stops it, and resends the action with
   // resolved promise to reducers
-  console.log('(In Reducer) Action received', action);
+  switch (action.type) {
+    case FETCH_WEATHER:// state.concat to create a new state, since .push since it would modify existing state.
+      return [action.payload.data, ...state]; // es6 syntax
+  }
   return state;
 }
